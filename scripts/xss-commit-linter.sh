222cf710e1ed9a054425cf50f7b2059644c81fcb
Michael Youngstrom <youngstrom.m@husky.neu.edu>
2019-01-04 17:30:32 -0500

    Test

--- scripts/xss-commit-linter.sh
+++ scripts/xss-commit-linter.sh
@@ -60,8 +60,10 @@ done
 current_branch_hash=`git rev-parse HEAD`
 
 if [ -z "${MAIN_COMMIT+x}" ]; then
-    # if commit is not set, get hash of current branch
-    MAIN_COMMIT="origin/master"
+    if [ -z "${TARGET_BRANCH+x}" ]; then
+        # if commit is not set and no target branch, get hash of current branch
+        MAIN_COMMIT="origin/master"
+    MAIN_COMMIT=${TARGET_BRANCH}
 fi
 
 merge_base_command="git merge-base $current_branch_hash $MAIN_COMMIT"
