11642b86d5b3fc8d619ac82df1ef8be83162fcb5
Troy Sankey <sankeytms@gmail.com>
2019-02-12 11:03:55 -0500

    Additionally logout from a settings list of extra logout URIs
    Currently, the LMS logout endpoint should iframe in the logout pages of
    all the IDAs you were logged into. In short, this was made possible with
    DOP because keeping track of the logout URIs and leaving a trail of
    evidence in the user cookies was part of what we added in our fork of
    DOP.  In the case of DOT, we don't have time or desire to fork DOT to
    mirror this behavior, so our stop-gap solution is to log out the user
    from a list of logout URIs in settings.
    
    (cherry picked from commit 10afe5e52f19e045863243e992f3433ce36b482a)

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -106,6 +106,8 @@ from lms.envs.common import (
     # Enterprise service settings
     ENTERPRISE_CATALOG_INTERNAL_ROOT_URL,
 
+    IDA_LOGOUT_URI_LIST,
+
     # Methods to derive settings
     _make_mako_template_dirs,
     _make_locale_paths,
