49e0f66801def945def1264d2b2a5dbc0487a91c
Cory Lee <cory@edx.org>
2019-03-06 17:18:39 -0500

    production.py should fallback to aws.py incase the plugin has not yet been migrated to use production.py

--- lms/envs/production.py
+++ lms/envs/production.py
@@ -33,6 +33,7 @@ from openedx.core.djangoapps.plugins import plugin_settings, constants as plugin
 from openedx.core.lib.derived import derive_settings
 from openedx.core.lib.logsettings import get_logger_config
 from xmodule.modulestore.modulestore_settings import convert_module_store_setting_if_needed
+from openedx.core.djangoapps.plugins import plugin_settings, constants as plugin_constants
 
 from .common import *
 
