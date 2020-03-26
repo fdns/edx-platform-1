8495213b075e7429a597b06065f8db3e0eea6a45
Felipe Espinoza <felipe.espinoza.r@uchile.cl>
2020-03-26 17:58:15 +0000

    eol: Adding default setting for reject registration pattern

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -3485,3 +3485,6 @@ USER_STATE_BATCH_SIZE = 5000
 from openedx.core.djangoapps.plugins import plugin_apps, plugin_settings, constants as plugin_constants
 INSTALLED_APPS.extend(plugin_apps.get_apps(plugin_constants.ProjectType.LMS))
 plugin_settings.add_plugins(__name__, plugin_constants.ProjectType.LMS, plugin_constants.SettingsType.COMMON)
+
+############## EOL #########################
+REGISTRATION_EMAIL_PATTERNS_DISALLOWED = None
