4a4755d103dff45917d8186c42b397cbac2f01f8
Felipe Espinoza <felipe.espinoza.r@uchile.cl>
2020-04-21 21:40:52 +0000

    Revert "change name permission"
    This reverts commit c690508b8300068c88e7d59abd78eddc095b2de4.

--- lms/djangoapps/instructor/views/instructor_dashboard.py
+++ lms/djangoapps/instructor/views/instructor_dashboard.py
@@ -115,10 +115,7 @@ def instructor_dashboard_2(request, course_id):
         'staff': bool(has_access(request.user, 'staff', course)),
         'forum_admin': has_forum_access(request.user, course_key, FORUM_ROLE_ADMINISTRATOR),
     }
-    
-    ### EOl - instructor_staff ###
-    access['uchile_instructor_staff'] = request.user.has_perm('uchileedxlogin.uchile_instructor_staff')
-    
+
     if not access['staff']:
         raise Http404()
 
